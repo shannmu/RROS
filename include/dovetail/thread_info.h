91f33b12c7ca6f72489cab78fb32d2916b1fa59d
shanmu <18801056930@163.com>
2024-10-23 04:11:42 +0000

    c: rros: define per-thread state information of rros

--- include/dovetail/thread_info.h
+++ include/dovetail/thread_info.h
@@ -8,6 +8,12 @@
  */
 
 struct oob_thread_state {
+	int preempt_count;
+
+	void *thread;
+	// struct rros_thread *thread;
+	void *subscriber;
+	// struct rros_subscriber *subscriber;
 };
 
 #endif /* !_DOVETAIL_THREAD_INFO_H */
